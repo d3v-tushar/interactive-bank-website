7ed0514fc5c3489370989a90bd80f50a031b2482
d3v-tushar <d3v.tushar@gmail.com>
2022-08-18 01:30:27 +0600

    Color Adjusted & Text Aligned

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     const email = emailFeild.value;
     const passwordFeild = document.getElementById('user-password');
     const password = passwordFeild.value;
-    if(email === 'tushar@github.com' && password === '00000000'){
+    if(email === 'tushar@github.com' && password === '00000000' || email === 'anika@bulbuli.com' && password === '88888888'){
         window.location.href = 'bank.html';
     }
     else{
