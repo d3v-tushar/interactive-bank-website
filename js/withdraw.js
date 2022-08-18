8b35c9ae4e5a84f96194bf2f25d8904c62697cb3
d3v-tushar <d3v.tushar@gmail.com>
2022-08-19 04:17:02 +0600

    Validation For Withdraw

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,19 +2,30 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawElement = document.getElementById('withdraw-field');
     const withdrawElementString = withdrawElement.value;
     const withdrawAmount = parseFloat(withdrawElementString);
+    withdrawElement.value = '';
+
+    if(isNaN(withdrawAmount)){
+        alert('Ener Numarical Vlaue Please')
+        return;
+    }
     // console.log(withdrawAmount);
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const withdrawTotalString = withdrawTotalElement.innerText;
     const withdrawTotal = parseFloat(withdrawTotalString);
-    // console.log(withdrawTotal);
-    const newWithdraw = withdrawTotal + withdrawAmount;
-    // console.log(newWithdraw);
-    withdrawTotalElement.innerText = newWithdraw;
+
+    // Balance
     const balanceTotalElement = document.getElementById('balance-total');
     const balanceTotalString = balanceTotalElement.innerText;
     const balanceTotal = parseFloat(balanceTotalString);
+
+    if(balanceTotal < withdrawAmount){
+        alert('Insufficiant Balance')
+        return;
+    }
+    const newWithdraw = withdrawTotal + withdrawAmount;
+    withdrawTotalElement.innerText = newWithdraw;
+
+    // Total balance
     const balance = balanceTotal - withdrawAmount;
-    console.log(balance);
     balanceTotalElement.innerText = balance;
-    withdrawElement.value = '';
 })
